3d7214c0e046a5cbf3bda3aaaf00240b210b7ebb
Ashar <sarah.eggleston@alliants.com>
2018-01-18 16:04:36 +0000

    Basic routing setup with config page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+
+import ConfigPage from './pages/ConfigPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a react project</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Router>
+          <Route exact path="/" component={ConfigPage} />
+        </Router>
       </div>
     );
   }
---
+++ src/pages/ConfigPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConfigPage = () => (
+  <div>
+    <h2>Config Page!</h2>
+  </div>
+)
+
+export default ConfigPage
