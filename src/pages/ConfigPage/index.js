8be2982e8fd05855787e11003d968e0877e35532
Ashar <segglestn@gmail.com>
2018-02-05 09:31:51 +0000

    Working

--- src/pages/ConfigPage/index.js
+++ src/pages/ConfigPage/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import ClientForm from '../../components/ClientForm'
-import Api from '../../components/Api'
-
+//import ClientForm from '../../components/ClientForm'
+//import Api from '../../components/Api'
+//import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 
 class ConfigPage extends React.Component {
   constructor(props) {
@@ -16,7 +17,22 @@ class ConfigPage extends React.Component {
     this.setState({clientKey: clientKeyValue});
   }
 
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+
   render() {
+    return (
+      <div>
+        <h2>Testing google login button</h2>
+        <GoogleLogin clientId="282752980559-bntufk9oeftfq204f9fu99ou2faou070.apps.googleusercontent.com"
+                   buttonText="Login" onSuccess={this.responseGoogle} onFailure={this.responseGoogle}/>,
+        document.getElementById('googleButton')
+      </div>
+    )
+  }
+
+  /*render() {
     return (
       <div>
         <h2>Config Page!</h2>
@@ -24,7 +40,7 @@ class ConfigPage extends React.Component {
         <Api clientKey={this.state.clientKey}/>
       </div>
     )
-  }
+  }*/
 }
 
 export default ConfigPage
